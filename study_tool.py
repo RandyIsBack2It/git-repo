79eb5b8e851dd10d85f19afb7593fa31ca2bc56a
Randy Caron <Randy.Caron@ymail.com>
2023-10-29 08:33:31 -0400

    Bug fixes for prompt menu. No second feature defined.

--- study_tool.py
+++ study_tool.py
@@ -1,78 +1,67 @@
 import openpyxl
 import re
 
+# Function for performing a keyword search in an Excel file
 def keyword_search():
-    # Define the filename of the Excel file
-    file_name = "study_tool.xlsx"
+    file_name = "study_tool.xlsx"  # Define the Excel file name
 
-    # Get user input for the keyword
-    print()  # Blank line added
-    keyword = input("Enter the keyword to search for: ")
-    print()  # Blank line added
+    keyword = input("Enter the keyword to search for: ")  # Get user input for the search keyword
+    print()  # Add a blank line here
+    pattern = r'\b' + re.escape(keyword) + r'\b'  # Prepare a regular expression pattern for the whole word match
 
-    # Prepare a regular expression pattern for the keyword as a whole word
-    pattern = r'\b' + re.escape(keyword) + r'\b'
-
-    # Open the Excel file
     try:
-        workbook = openpyxl.load_workbook(file_name)
+        workbook = openpyxl.load_workbook(file_name)  # Open the Excel file
         sheet = workbook.active
+        matching_rows = []  # Initialize a list to store rows with matching data
+        headers = None  # Initialize variable to store column headers
 
-        # Initialize a list to store matching rows
-        matching_rows = []
-
-        # Initialize headers
-        headers = None
-
-        # Iterate through each row in the sheet
         for row in sheet.iter_rows(values_only=True):
             if not headers:
-                headers = row  # First row is assumed to be the headers
+                headers = row  # Store the first row as headers
                 continue
 
-            # Check if the keyword (case-insensitive, whole word) is in any of the cells in the row
             if any(cell is not None for cell in row):
                 for cell in row:
                     if cell is not None and re.search(pattern, str(cell), re.I):
-                        matching_rows.append(row)
+                        matching_rows.append(row)  # Add the row to matching rows if it contains the keyword
                         break
 
-        # Display the matching rows with values in separate rows
         if matching_rows:
-            print()  # Blank line added
+            # Display matching rows with column headers
             for row in matching_rows:
                 if any(cell is not None for cell in row):
                     for header, cell in zip(headers, row):
                         if cell is not None:
                             print(f"{header}: {cell}")
                     print()
+            print()  # Add a blank line here
         else:
             print("No matching rows found for the keyword.")
 
-        # Close the Excel file
-        workbook.close()
+        workbook.close()  # Close the Excel file
 
     except FileNotFoundError:
         print(f"File '{file_name}' not found.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
-# Menu loop
+# Main menu loop
 while True:
     print("\nSelect an Option:")
-    print("  1. Keyword Search")  # Added blank space
+    print("  1. Keyword Search")
     print("  2. Additional Feature (Not implemented yet)")
-    print("  3. Exit")  # Added blank space
-    print()  # Blank line added
-    choice = input("Select an option (1/2/3): ")
-    print()  # Blank line added
+    print("  3. Exit")
+    print()  # Add a blank line
+    choice = input("Select an option (1/2/3): ")  # Get the user's choice
+    print()  # Add a blank line here
 
     if choice == '1':
-        keyword_search()
+        keyword_search()  # Call the keyword_search function for option 1
     elif choice == '2':
-        print("This feature is not implemented yet.")
+        print("This feature is not implemented yet.")  # Display a message for option 2
     elif choice == '3':
-        print("Exiting the script. Goodbye!\n")  # Blank line added
+        print("Exiting the script. Goodbye!")  # Display a goodbye message
+        print()  # Add a blank line here
         break
     else:
-        print("Invalid choice. Please select a valid option (1/2/3).")
+        print("Invalid choice. Please select a valid option (1/2/3).")  # Display an error message for invalid choices
