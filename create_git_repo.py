04a900b4946cd0363d48c0abe8e941e73a37e799
RandyIsBack2It <randy.caron@ymail.com>
2023-10-29 11:42:58 -0400

    Preparing branches for merge.

--- create_git_repo.py
+++ create_git_repo.py
@@ -1,6 +1,10 @@
+import sys
 import subprocess
 import os
 import shutil
+
+# Reset standard output to the default
+sys.stdout = sys.__stdout__
 
 # GitHub username
 github_username = "RandyIsBack2It"
@@ -18,73 +22,74 @@ def list_files_in_directory(directory):
 
 def create_git_repository(repo_name, selected_file):
     cwd = os.getcwd()  # Get the current working directory
-    repo_path = os.path.join(cwd, repo_name)  # Use CWD as the repository directory
 
-    if os.path.exists(repo_path):
-        print(f"Repository '{repo_name}' already exists in this directory.")
-    else:
-        os.makedirs(repo_path)  # Create the repository directory if it doesn't exist
-        subprocess.run(["git", "init", repo_path])
+    # Use the selected file's name (without extension) as the local folder name
+    local_folder_name = os.path.splitext(selected_file)[0]
+    local_folder_path = os.path.join(cwd, local_folder_name)
 
-        # Set Git global username and email programmatically
-        subprocess.run(["git", "config", "--global", "user.name", "RandyIsBack2It"])
-        subprocess.run(["git", "config", "--global", "user.email", "randy.caron@ymail.com"])
+    if os.path.exists(local_folder_path):
+        print(f"Local folder '{local_folder_name}' already exists.")
+        return
 
-        print(f"Repository '{repo_name}' created successfully.")
+    # Create the local folder
+    os.makedirs(local_folder_path)
 
-        # Automatically copy the selected file to the repository
-        src_file_path = os.path.join(cwd, selected_file)  # Use CWD as the source directory
-        dst_file_path = os.path.join(repo_path, selected_file)
-        shutil.copy(src_file_path, dst_file_path)
-        print(f"File '{selected_file}' added to the repository.")
+    # Initialize a Git repository in the local folder
+    subprocess.run(["git", "init", local_folder_path])
 
-        # Create the "readme.md" file
-        md_file_name = "readme.md"
-        md_file_path = os.path.join(repo_path, md_file_name)
-        with open(md_file_path, "w") as md_file:
-            md_file.write(f"# {repo_name}\n")  # Add repository name as the first line
+    # Set Git global username and email programmatically
+    subprocess.run(["git", "config", "--global", "user.name", "RandyIsBack2It"])
+    subprocess.run(["git", "config", "--global", "user.email", "randy.caron@ymail.com"])
 
-            # Read the script file and copy comment lines to the markdown file
-            script_file_path = os.path.join(cwd, selected_file)  # Use CWD as the source directory
-            with open(script_file_path, "r") as script_file:
-                for line in script_file:
-                    if line.strip().startswith("#"):
-                        md_file.write(line)
+    print(f"Local folder '{local_folder_name}' created successfully.")
 
-        print(f"Markdown file '{md_file_name}' created in the repository.")
+    # Automatically copy the selected file to the local folder
+    src_file_path = os.path.join(cwd, selected_file)
+    dst_file_path = os.path.join(local_folder_path, selected_file)
+    shutil.copy(src_file_path, dst_file_path)
+    print(f"File '{selected_file}' added to the local folder.")
 
-        # Copy files with the same base name (ignoring the extension) to the repository
-        selected_file_base = os.path.splitext(selected_file)[0]
-        for file in os.listdir(cwd):
-            if os.path.isfile(os.path.join(cwd, file)) and os.path.splitext(file)[0] == selected_file_base:
-                shutil.copy(os.path.join(cwd, file), os.path.join(repo_path, file))
-                print(f"File '{file}' with the same base name copied to the repository.")
+    # Create the "readme.md" file
+    md_file_name = "readme.md"
+    md_file_path = os.path.join(local_folder_path, md_file_name)
+    with open(md_file_path, "w") as md_file:
+        md_file.write(f"# {local_folder_name}\n")  # Add folder name as the first line
 
-        commit_message = input("Enter the commit message (max 150 characters): ")
-        if len(commit_message) > 150:
-            print("Commit message is too long. Please keep it under 150 characters.")
-            return
+        # Read the script file and copy comment lines to the markdown file
+        script_file_path = os.path.join(cwd, selected_file)
+        with open(script_file_path, "r") as script_file:
+            for line in script_file:
+                if line.strip().startswith("#"):
+                    md_file.write(line)
 
-        subprocess.run(["git", "add", "."], cwd=repo_path)
-        subprocess.run(["git", "commit", "-m", commit_message], cwd=repo_path)
+    print(f"Markdown file '{md_file_name}' created in the local folder.")
 
-        print(f"Changes committed with the message: '{commit_message}'")
+    commit_message = input("Enter the commit message (max 150 characters): ")
+    if len(commit_message) > 150:
+        print("Commit message is too long. Please keep it under 150 characters.")
+        return
 
-        # Add a remote repository (GitHub)
-        subprocess.run(["git", "remote", "add", "origin", f"https://{github_username}:{github_token}@github.com/{github_username}/{repo_name}.git"], cwd=repo_path)
+    # Add and commit changes in the local repository
+    subprocess.run(["git", "add", "."], cwd=local_folder_path)
+    subprocess.run(["git", "commit", "-m", commit_message], cwd=local_folder_path)
 
-        # Push changes to the remote repository
-        subprocess.run(["git", "push", "-u", "origin", "main"], cwd=repo_path)  # Change "main" to your branch name
+    print(f"Changes committed with the message: '{commit_message}'")
+
+    # Add a remote repository (GitHub)
+    remote_repo_name = "scripts_base"
+    subprocess.run(["git", "remote", "add", "origin", f"https://{github_username}:{github_token}@github.com/{github_username}/{remote_repo_name}.git"], cwd=local_folder_path)
+
+    # Push changes to the remote repository
+    subprocess.run(["git", "push", "-u", "origin", "main"], cwd=local_folder_path)  # Change "main" to your branch name
 
 if __name__ == "__main__":
-    files = list_files_in_directory(os.getcwd())  # List files in the current working directory (CWD)
+    files = list_files_in_directory(os.getcwd())  # List files in the current working directory
     choice = input("Choose the file that you are creating a repository for (enter the number): ")
     try:
         choice = int(choice)
         if 1 <= choice <= len(files):
             selected_file = files[choice - 1]
-            repo_name = os.path.splitext(selected_file)[0]  # Use the base name (omit the extension)
-            create_git_repository(repo_name, selected_file)
+            create_git_repository("scripts_base", selected_file)
         else:
             print("Invalid choice. Please select a valid number.")
     except ValueError:
